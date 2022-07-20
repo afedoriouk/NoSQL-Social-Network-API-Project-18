0cb0fd678ab94e1620f2e1a252fe6d43c1a15f93
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-20 14:59:35 -0400

    posts

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,20 +1,4 @@
 const router = require("express").Router();
-// const getUsers = require("../../controllers/user-controller.js")
-//   getSingleUser,
-//   createUser,
-//   updateUser,
-//   deleteUser,
-//   addFriend,
-//   removeFriend,
- 
 
-// // /api/users
-// router.route("/").get(getUsers).post(createUser);
-
-// // /api/users/:userId
-// router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
-
-// // /api/users/:userId/friends/:friendId
-// router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
